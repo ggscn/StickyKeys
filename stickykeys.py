862f2e9d61bfa8cef476838dc6a7c5a02b88fda7
ggascon <greg.gascon@gmail.com>
2020-07-09 18:20:18 +0000

    keyboard layout

--- stickykeys.py
+++ stickykeys.py
@@ -3,20 +3,36 @@ from nltk.tokenize import word_tokenize
 from random import choice
 import nltk
 
+@dataclass
+class Keyboard:
+    layout: list = [
+        '1234567890',
+        'qwertyuiop',
+        'asdfghjkl',
+        'zxcvbnm'
+    ]
+
+
 class TypoText:
     def __init__(self, text: str):
         self.text = text
         self.max_swap_distance = 3
         self.tokens = self.tokenize(text)
 
-    def tokenize(self, text):
+    def tokenize(self, text: str) -> list:
         tokens = word_tokenize(text)
         return tokens
     
-    def get_keyboard_layout(self: str):
-        pass
+    def mispress(self, char: str):
+        keyboard_layout = Keyboard.layout
+        char_row_index = [i for i,x in enumerate(
+            keyboard_layout) if char in x][0]
+        char_inner_row_index = keyboard_layout[
+            char_row_index].index(char)
+        
+        
 
-    def swap_keys(self, token):
+    def swap_keys(self, token: str):
         max_distance = self.max_swap_distance
         token_range = list(range(len(token)))
         swap_source_index = choice(token_range)
@@ -36,9 +52,9 @@ class TypoText:
         
         return token
 
-    def run(self):
+    def process(self):
         token = self.swap_keys(self.tokens[3])
         self.tokens[3] = token
         print(' '.join(self.tokens))
 
-TypoText('This is a sentence').run()
+TypoText('This is a sentence').process()
